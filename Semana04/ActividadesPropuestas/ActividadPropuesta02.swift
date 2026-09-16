e9436511f589f806365023a6b5f00125a9e3cdd1
Gean Pierre Ayala <gean.ayala.dev@gmail.com>
2026-09-16 07:56:57 -0500

    feat: Actividad 02 - subclase ClienteNatural

--- Semana04/ActividadesPropuestas/ActividadPropuesta02.swift
+++ Semana04/ActividadesPropuestas/ActividadPropuesta02.swift
@@ -29,3 +29,21 @@ class Cliente {
         print("💰 Monto mínimo de apertura: S/ \(String(format: "%.2f", montoMinimoApertura))")
     }
 }
+
+class ClienteNatural: Cliente {
+    let nombreCompleto: String
+    let dni: String
+
+    init(codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double, nombreCompleto: String, dni: String) {
+        self.nombreCompleto = nombreCompleto
+        self.dni = dni
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro, numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("👤 Cliente Natural:")
+        print("Nombre: \(nombreCompleto)")
+        print("DNI: \(dni)")
+        super.mostrarDatos()
+    }
+}
